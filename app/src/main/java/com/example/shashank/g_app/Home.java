b0d752a92663158f7d54c23e63a7c99f49e71061
storyteller <shashanksharma580S@gmail.com>
2017-03-15 20:58:53 +0530

    login complete

--- app/src/main/java/com/example/shashank/g_app/Home.java
+++ app/src/main/java/com/example/shashank/g_app/Home.java
@@ -1,25 +1,150 @@
 package com.example.shashank.g_app;
 
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.os.Handler;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home extends AppCompatActivity {
+
+    android.app.ActionBar actionBar;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private int[] tabIcons = {
+            R.drawable.three,
+            R.drawable.two,
+            R.drawable.ic_home_black_24dp1
+    };
+
+    Boolean check = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Button button =(Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Snackbar.make(view,"created by:-shashank",Snackbar.LENGTH_INDEFINITE)
-                .setAction("Action",null).show();
-            }
-        });
+        actionBar = getActionBar();
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
     }
+
+    private void setupTabIcons() {
+
+        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_home_black_24dp1, 0, 0);
+        tabLayout.getTabAt(0).setCustomView(tabOne);
+
+        TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabTwo.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.two, 0, 0);
+        tabLayout.getTabAt(1).setCustomView(tabTwo);
+
+        TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabThree.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.three, 0, 0);
+        tabLayout.getTabAt(2).setCustomView(tabThree);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new OneFragment(), "ONE");
+        adapter.addFrag(new TwoFragment(), "TWO");
+        adapter.addFrag(new ThreeFragment(), "THREE");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater in = getMenuInflater();
+        in.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.logout:
+             SharedPreferences  sp=getSharedPreferences("login",1);
+                SharedPreferences.Editor e=sp.edit();
+                e.putString("id",null);
+                e.putString("pass",null);
+                e.commit();
+                startActivity(new Intent(Home.this,Splash.class));
+                finish();
+                return(true);
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+    @Override
+    public void onBackPressed() {
+
+        if(check)
+        {
+            super.onBackPressed();
+            return;
+        }
+        this.check=true;
+        Toast.makeText(this,"press back again to exit",Toast.LENGTH_SHORT).show();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                check=false;
+            }
+        },2000);
+    }
+
 }
+
+
+
+
--- app/src/main/java/com/example/shashank/g_app/Splash.java
+++ app/src/main/java/com/example/shashank/g_app/Splash.java
@@ -1,32 +1,111 @@
 package com.example.shashank.g_app;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class Splash extends AppCompatActivity {
 
-    @Override
+    boolean check =false;
+    ImageView iv;
+    EditText idd,passs;
+    LinearLayout ll;
+    Button btn;
+    String id,pass,i_d,k_ey;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-        ImageView iv=(ImageView) findViewById(R.id.img);
+        SharedPreferences sp=getSharedPreferences("login",1);
+         i_d=sp.getString("id","def");
+         k_ey=sp.getString("key","def");
+
+        passs= (EditText) findViewById(R.id.password);
+        idd= (EditText) findViewById(R.id.input_number);
+        ll=(LinearLayout)findViewById(R.id.login);
+        btn=(Button) findViewById(R.id.loginbutton);
+        iv=(ImageView) findViewById(R.id.img);
         Animation an= AnimationUtils.loadAnimation(getBaseContext(),R.anim.clock);
         iv.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if((i_d.equals("14SCSE106002")) && (k_ey.equals("asd")))//shared preference chek on database and bring data related to saved key value pair
+                {
+                    startActivity(new Intent(Splash.this,Home.class));
+                }
+                else
+                    {
+                Animation an2 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.anim);
+                an2.setFillAfter(true);
+                iv.startAnimation(an2);
+                an2.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                    }
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        ll.setVisibility(View.VISIBLE);
+                    }
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                    }
+                });
+            }}
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
 
-        Animation an2= AnimationUtils.loadAnimation(getBaseContext(),R.anim.anim);
-        iv.startAnimation(an2);
+    }
 
-        new Timer().schedule(new TimerTask() {
+    public void fun(View v){
+      id= idd.getText().toString();
+        pass=passs.getText().toString();
+        if((id.equals("14SCSE106002")) && (pass.equals("asd")))//database connectivity to bring info
+        {
+            SharedPreferences sp= getSharedPreferences("login",1);
+            SharedPreferences.Editor e=sp.edit();
+            e.putString("id",id);
+            e.putString("key",pass);
+            e.commit();
+            startActivity(new Intent(this,Home.class));
+        }
+        else{
+            Toast.makeText(this,"wrong id ,password", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(check)
+        {
+            super.onBackPressed();
+            return;
+        }
+        this.check=true;
+        Toast.makeText(this,"press back again to exit",Toast.LENGTH_SHORT).show();
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i=new Intent(getApplicationContext(),Home.class);
-                startActivity(i);
+                check=false;
             }
-        },9000);
-
+        },2000);
     }
 
     @Override
